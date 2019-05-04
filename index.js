1f8c209facfb4bb485ddfb8627ba220783171a09
Neil Williams <Neil.Williams227@gmail.com>
2019-05-04 16:54:32 -0400

    class based implementation

--- index.js
+++ index.js
@@ -6,101 +6,87 @@ const apiKey5 = "lDT5Ka1ymuq0o1xOaJbO7UGJmKTkPnG4"; // mines
 
 const proxy = "https://cors-anywhere.herokuapp.com/";
 
-//query paramaters search function
-let getUrlParams = () => {
-  let url = new URLSearchParams(location.search.substring(1));
-  let query = {
-    zip: "",
-    date: ""
+class WeatherApp {
+  getUrlParams = () => {
+    let url = new URLSearchParams(location.search.substring(1));
+    let query = {
+      zip: "",
+      date: ""
+    };
+
+    url.forEach(function(value, key) {
+      switch (key) {
+        case "zipcode":
+          query.zip = value;
+          break;
+        case "date":
+          query.date = value;
+          break;
+      }
+    });
+
+    return query;
   };
 
-  console.log("this should be first");
+  getCityValues = async () => {
+    let params = this.getUrlParams();
+    let url = `http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=${apiKey2}&q=${
+      params.zip
+    }`;
 
-  url.forEach(function(value, key) {
-    switch (key) {
-      case "zipcode":
-        query.zip = value;
-        break;
-      case "date":
-        query.date = value;
-        break;
+    // wrap in a if statement to execute if there are params
+    let data = await (await fetch(`${proxy}${url}`)).json();
+
+    let cityValues = {
+      cityToken: data[0].Key,
+      cityName: data[0].LocalizedName
+    };
+    return cityValues;
+  };
+
+  dataApi = async () => {
+    const key = await this.getCityValues();
+    const api = `${proxy}
+      http://dataservice.accuweather.com/forecasts/v1/daily/5day/${
+        key.cityToken
+      }?apikey=${apiKey3}
+      `;
+    return api;
+  };
+
+  //fetches the Data and display
+  getData = async () => {
+    //   fetch(api)
+    // let api = await this.dataApi();
+
+    let res = await fetch("data.json");
+    let data = res.json();
+
+    return data;
+  };
+
+  displayData = async () => {
+    let queryDate;
+    let query = await this.getUrlParams();
+    let cityName = await this.getCityValues();
+    if (query.date === "") {
+      queryDate = new Date();
+    } else {
+      queryDate = new Date(query.date);
     }
-  });
 
-  return query;
-};
+    let data = await this.getData();
+    let output;
+    let header;
+    let todayDate = new Date();
 
-// global variable
-let result = {};
-
-// query Zip Code
-let queryZipCode = async () => {
-  console.log("this should be second");
-
-  let params = getUrlParams();
-  console.log(params);
-  let url = `http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=${apiKey3}&q=${
-    params.zip
-  }`;
-
-  // wrap in a if statement to execute if there are params
-  let data = await (await fetch(`${proxy}${url}`)).json();
-  //   console.log(data);
-
-  let cityValues = {
-    cityToken: data[0].Key,
-    cityName: data[0].LocalizedName
-  };
-  return cityValues;
-};
-
-let changeAPI = async () => {
-  console.log("this should be third");
-
-  const key = await queryZipCode();
-  console.log(key);
-  const api = `${proxy}
-http://dataservice.accuweather.com/forecasts/v1/daily/5day/${
-    key.cityToken
-  }?apikey=${apiKey3}
-`;
-  //   console.log(api);
-  return api;
-};
-
-// fake data for now
-let getData = async () => {
-  console.log("this should be four");
-  console.log(result);
-  let queryDate;
-  let api = await changeAPI();
-  let query = await getUrlParams();
-  let cityName = await queryZipCode();
-  console.log(cityName);
-  console.log(typeof query.date);
-  if (query.date === "") {
-    queryDate = new Date();
-  } else {
-    queryDate = new Date(query.date);
-  }
-
-  fetch(api)
-    //   fetch("data.json")
-    .then(res => res.json())
-    .then(data => {
-      //   console.log(data);
-      let output;
-      let header;
-      let todayDate = new Date();
-
-      const { DailyForecasts } = data;
+    const { DailyForecasts } = data;
+    try {
       DailyForecasts.forEach(temp => {
         let forecastDate = new Date(temp.Date);
-        console.log(forecastDate);
         if (queryDate.getDay() < todayDate.getDate()) {
           output = "";
           header = "";
-          console.log(queryDate, todayDate);
         } else if (queryDate <= forecastDate) {
           header = `
             <div class="container">
@@ -115,7 +101,7 @@ let getData = async () => {
           output += `
             <div class="forecast">
                 <div class="box">
-                    <span class="day"> ${findDate(temp.Date)}  </span>    
+                    <span class="day"> ${this.findDate(temp.Date)}  </span>    
                     <div class="temp">
                         <span class="icon">
                         <img src='https://apidev.accuweather.com/developers/Media/Default/WeatherIcons/${
@@ -143,42 +129,43 @@ let getData = async () => {
       });
       document.querySelector(".days").innerHTML = output;
       document.querySelector(".city").innerHTML = header;
-    })
-    .catch(err => console.log(err));
-};
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-getData();
+  findDate = date => {
+    const convertDate = new Date(date);
 
-// query the date
+    switch (convertDate.getDay()) {
+      case 0:
+        return "Sunday";
+        break;
+      case 1:
+        return "Monday";
+        break;
 
-let findDate = date => {
-  const convertDate = new Date(date);
+      case 2:
+        return "Tuesday";
+        break;
 
-  switch (convertDate.getDay()) {
-    case 0:
-      return "Sunday";
-      break;
-    case 1:
-      return "Monday";
-      break;
+      case 3:
+        return "Wednesday";
+        break;
 
-    case 2:
-      return "Tuesday";
-      break;
+      case 4:
+        return "Thursday";
+        break;
 
-    case 3:
-      return "Wednesday";
-      break;
+      case 5:
+        return "Friday";
+        break;
+      case 6:
+        return "Saturday";
+        break;
+    }
+  };
+}
 
-    case 4:
-      return "Thursday";
-      break;
-
-    case 5:
-      return "Friday";
-      break;
-    case 6:
-      return "Saturday";
-      break;
-  }
-};
+let App = new WeatherApp();
+App.displayData();
